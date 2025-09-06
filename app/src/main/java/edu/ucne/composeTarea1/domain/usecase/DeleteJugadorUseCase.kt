cca1055d14735c7be87a108e8721ccb2f71b2531
Danielvi <danielvilopez@gmail.com>
2025-09-05 22:50:02 -0400

    Implementar caso de uso para eliminar jugador

---
+++ app/src/main/java/edu/ucne/composeTarea1/domain/usecase/DeleteJugadorUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.composeTarea1.domain.usecase
+
+import edu.ucne.composeTarea1.domain.repository.JugadorRepository
+import javax.inject.Inject
+
+class DeleteJugadorUseCase @Inject constructor(
+    private val repository: JugadorRepository
+) {
+    suspend operator fun invoke(id: Int) = repository.deleteJugador(id)
+}
