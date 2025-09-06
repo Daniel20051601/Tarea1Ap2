a44e9b87162a427ad3e07ccc8a28692c9df6d121
Danielvi <danielvilopez@gmail.com>
2025-09-05 22:48:52 -0400

    Configurar inyección de dependencias con Hilt

---
+++ app/src/main/java/edu/ucne/composeTarea1/di/AppModule.kt
@@ -0,0 +1,19 @@
+package edu.ucne.composeTarea1.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.composeTarea1.domain.repository.JugadorRepository
+import edu.ucne.composeTarea1.tareas.repository.JugadorRepositoryImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModule {
+    @Binds
+    @Singleton
+    abstract fun bindJugadorRepository(
+        jugadorRepositoryImpl: JugadorRepositoryImpl
+    ): JugadorRepository
+}
