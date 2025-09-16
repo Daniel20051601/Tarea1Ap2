80b7547a47be69f6e414b5e51797dd658fe7fd29
Danielvi <danielvilopez@gmail.com>
2025-09-15 23:35:57 -0400

    Agrega PartidaRepository y PartidaRepositoryImpl a AppModule

--- app/src/main/java/edu/ucne/composeTarea1/di/AppModule.kt
+++ app/src/main/java/edu/ucne/composeTarea1/di/AppModule.kt
@@ -5,7 +5,9 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.composeTarea1.domain.repository.JugadorRepository
+import edu.ucne.composeTarea1.domain.repository.PartidaRepository
 import edu.ucne.composeTarea1.tareas.repository.JugadorRepositoryImpl
+import edu.ucne.composeTarea1.tareas.repository.PartidaRepositoryImpl
 import javax.inject.Singleton
 
 @Module
@@ -16,4 +18,10 @@ abstract class AppModule {
     abstract fun bindJugadorRepository(
         jugadorRepositoryImpl: JugadorRepositoryImpl
     ): JugadorRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindPartidaRepository(
+        partidaRepositoryImpl: PartidaRepositoryImpl
+    ): PartidaRepository
 }
