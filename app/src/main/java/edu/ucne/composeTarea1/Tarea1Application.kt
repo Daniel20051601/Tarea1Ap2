0d5e529cd2549a846c5f6b20953e1156ea10ae14
Danielvi <danielvilopez@gmail.com>
2025-09-21 20:04:55 -0400

    Elimina la inicialización del jugador por defecto en Tarea1Application.kt

--- app/src/main/java/edu/ucne/composeTarea1/Tarea1Application.kt
+++ app/src/main/java/edu/ucne/composeTarea1/Tarea1Application.kt
@@ -14,28 +14,4 @@ import javax.inject.Inject
 
 @HiltAndroidApp
 class Tarea1Application : Application(){
-    @Inject
-    lateinit var jugadorRepository: JugadorRepository
-
-    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-
-    override fun onCreate() {
-        super.onCreate()
-        setupDefaultPlayer()
-    }
-    private fun setupDefaultPlayer() {
-        applicationScope.launch {
-            val prefs = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
-            val isFirstLaunch = prefs.getBoolean("is_first_launch", true)
-
-            if (isFirstLaunch) {
-
-                val yoPlayer = jugadorRepository.getJugadorByName("Yo")
-                if (yoPlayer == null) {
-                    jugadorRepository.save(Jugador(nombres = "Yo", partidas = 0))
-                }
-                prefs.edit().putBoolean("is_first_launch", false).apply()
-            }
-        }
-    }
 }
