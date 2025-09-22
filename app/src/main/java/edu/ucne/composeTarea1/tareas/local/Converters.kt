f49c1bf87408c9f051de2dcd716c1a14f3aaba1f
Danielvi <danielvilopez@gmail.com>
2025-09-21 20:05:37 -0400

    Agrega convertidores de tipo para la serialización del tablero en la base de datos

---
+++ app/src/main/java/edu/ucne/composeTarea1/tareas/local/Converters.kt
@@ -0,0 +1,26 @@
+package edu.ucne.composeTarea1.tareas.local
+
+import androidx.room.TypeConverter
+import edu.ucne.composeTarea1.tareas.Presentation.Partida.Player
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class Converters {
+    @TypeConverter
+    fun fromBoardToString(board: List<Player?>): String {
+        val serializableList = board.map { it?.name }
+        return Json.encodeToString(serializableList)
+    }
+
+    @TypeConverter
+    fun fromStringToBoard(jsonString: String): List<Player?> {
+        val serializableList = Json.decodeFromString<List<String?>>(jsonString)
+        return serializableList.map { name ->
+            when (name) {
+                "X" -> Player.X
+                "O" -> Player.O
+                else -> null
+            }
+        }
+    }
+}
