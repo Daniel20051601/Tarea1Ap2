d0f49a8932620c1c2a638647f5438e04b31f29d0
Danielvi <danielvilopez@gmail.com>
2025-09-24 14:08:47 -0400

    Agrega LogroEntity y LogroDao a la base de datos

--- app/src/main/java/edu/ucne/composeTarea1/tareas/local/AppDataBase.kt
+++ app/src/main/java/edu/ucne/composeTarea1/tareas/local/AppDataBase.kt
@@ -5,19 +5,23 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import edu.ucne.composeTarea1.tareas.local.Jugador.JugadorDao
 import edu.ucne.composeTarea1.tareas.local.Jugador.JugadorEntity
+import edu.ucne.composeTarea1.tareas.local.Logro.LogroDao
+import edu.ucne.composeTarea1.tareas.local.Logro.LogroEntity
 import edu.ucne.composeTarea1.tareas.local.Partida.PartidaDao
 import edu.ucne.composeTarea1.tareas.local.Partida.PartidaEntity
 
 @Database(
     entities = [
         JugadorEntity::class,
-        PartidaEntity::class
+        PartidaEntity::class,
+        LogroEntity::class
     ],
-    version = 7,
+    version = 10,
     exportSchema = false,
 )
 @TypeConverters(Converters::class)
 abstract class AppDataBase : RoomDatabase() {
     abstract fun jugadorDao(): JugadorDao
     abstract fun PartidaDao(): PartidaDao
+    abstract fun logroDao(): LogroDao 
 }
