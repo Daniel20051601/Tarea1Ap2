eefff437d21c9c9e7a76725537a92d8dda74eb3f
Danielvi <danielvilopez@gmail.com>
2025-09-24 14:57:37 -0400

    Agrega LogroEntity y LogroDao a la base de datos

--- app/src/main/java/edu/ucne/composeTarea1/di/AppModule.kt
+++ app/src/main/java/edu/ucne/composeTarea1/di/AppModule.kt
@@ -2,11 +2,16 @@ package edu.ucne.composeTarea1.di
 
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.composeTarea1.domain.repository.JugadorRepository
+import edu.ucne.composeTarea1.domain.repository.LogroRepository
 import edu.ucne.composeTarea1.domain.repository.PartidaRepository
+import edu.ucne.composeTarea1.tareas.local.AppDataBase
+import edu.ucne.composeTarea1.tareas.local.Logro.LogroDao
 import edu.ucne.composeTarea1.tareas.repository.JugadorRepositoryImpl
+import edu.ucne.composeTarea1.tareas.repository.LogroRepositoryImpl
 import edu.ucne.composeTarea1.tareas.repository.PartidaRepositoryImpl
 import javax.inject.Singleton
 
@@ -24,4 +29,10 @@ abstract class AppModule {
     abstract fun bindPartidaRepository(
         partidaRepositoryImpl: PartidaRepositoryImpl
     ): PartidaRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindLogroRepository(
+        logroRepositoryImpl: LogroRepositoryImpl
+    ): LogroRepository
 }
